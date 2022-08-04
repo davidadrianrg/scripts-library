7d784c411d61d3bc8f25a9996c813f34bf2f1ee8
David Adrian Rodriguez Garcia <davidadrianrg@gmail.com>
2022-08-04 16:27:35 +0200

    Fix some issues with windows startup script

--- startup_windows.ps1
+++ startup_windows.ps1
@@ -4,16 +4,16 @@ Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManage
 # Avoid confirmation to install packages
 choco feature enable -n allowGlobalConfirmation
 # Install the programs using chocolatey package manager, must be an administrative shell
-choco install 7zip bitwarden foxitreader epicgameslauncher steam-client git vscode microsoft-teams firefox python qbittorrent rpi-imager spotify telegram whatsapp vlc cura-new croc
+choco install -y 7zip bitwarden adobereader epicgameslauncher steam-client git vscode jre8 microsoft-teams brave python qbittorrent rpi-imager spotify telegram whatsapp vlc croc nmap filezilla
 
 # Install windows package manager to install apps from microsoft store
-Invoke-WebRequest -Uri "https://github.com/microsoft/winget-cli/releases/download/v-0.3.11102-preview/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.appxbundle" -OutFile "$Env:temp\wpm.appxbundle"
+Add-AppxPackage "https://github.com/microsoft/winget-cli/releases/download/v1.3.2091/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
 Add-AppxPackage "https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx"
-Add-AppxPackage -Path "$Env:temp\wpm.appxbundle"
 
 # Install store apps with winget command
 winget install ubuntu
 winget install "windows terminal"
+winget install discord
 
 # Install WSL2.0
 Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
